9bcf7e96244270c8b405fde4701096bb99a0b5c5
shellphon <dont923@gmail.com>
2015-01-10 16:17:01 +0800

    change

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-function test(){console.log(1);}
+  (function($){
+    var ul = $('#ul_guide'),
+        tabParent = $('#tab_content');
+        $('#ul_guide li a').click(function(){
+          var _this = $(this),
+              curActive = ul.find('.active').eq(0);
+              curActive.removeClass('active');
+              _this.parents('li').addClass('active');
+              tabParent.find('.active').eq(0).removeClass('active');
+              $('#'+_this.attr('data-tabid')).addClass('active');
+        });
+  })(jQuery);
