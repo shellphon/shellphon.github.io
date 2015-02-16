da4c5f124f6dea91f9e16cceadd21cda12d64286
shellphon <dont923@gmail.com>
2015-02-16 11:16:23 +0800

    typeeffect

--- js/main.js
+++ js/main.js
@@ -12,4 +12,26 @@
     $('.show-ds').click(function(){
       $('.ds-thread').slideToggle();
     });
+    var typeEffect = function(dom,time){//打字效果，
+      var _obj = $(dom),
+          txt = _obj.text(),
+          len = txt.length;
+          _obj.text('');
+      function write(pos){
+        var letter = txt.substr(pos,1);
+        if(pos<len){
+          console.log(pos,len,letter,txt)
+          _obj.append(letter);
+          pos++;
+          setTimeout(function(){
+            write(pos); 
+          },time);
+        }
+      }
+      write(0);
+    }
+    setTimeout(function(){
+      typeEffect('.div-title h3', 88);
+      typeEffect('#tab_home p', 88);
+    },500);
   })(jQuery);
